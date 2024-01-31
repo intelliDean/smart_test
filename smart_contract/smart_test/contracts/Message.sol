dca16c51483b7aa922e8e48825f6c216a3240b92
intelliDean <intelliDean@gmail.com>
2024-01-31 17:28:24 +0100

    smart contract deployed

---
+++ smart_contract/smart_test/contracts/Message.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Message {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+}
